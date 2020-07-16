e2fafe38b0b52c135325da654bd59f177659d6ba
splingaire <splingaire.marvyn@gmail.com>
2020-07-16 15:48:25 +0200

    summit

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -3,20 +3,28 @@ let qrcode = document.querySelector("#qrcode");
 let valueFormation = document.querySelector("#formation");
 let valuePersonnel = document.querySelector("#personnel");
 let valueRdv = document.querySelector("#rdv");
+let inputNom = document.querySelector("#nom");
+let inputPrenom = document.querySelector("#prenom");
+let inputMail =  document.querySelector("#mail");
 let formationBlock = document.querySelector(".formationBlock");
 let personnelBlock = document.querySelector(".personnelBlock");
 let qrcodeBlock = document.querySelector(".qrcodeBlock");
 let container = document.querySelector(".yes");
+let perso = document.querySelector(".perso");
+let forma = document.querySelector(".forma");
+let messageEror = [
+    document.querySelector(".messageNom"),
+    document.querySelector(".messagePrenom"),
+    document.querySelector(".messageMail")
+];
 let listInput = [
     document.querySelector("#nom"),
     document.querySelector("#prenom"),
     document.querySelector("#mail"),
     document.querySelector(".btnSubmit")
 ];
-
 let dateMtn = new Date();
-let perso = null;
-let estRdvOuAutre = true;
+let message = " Champ obligatoire veuillez le remplir * ";
 
 //------------------------------------------------- GET -----------------------------------------------------//
     // recupere les donnees de l'api des formations
@@ -42,6 +50,13 @@ let estRdvOuAutre = true;
     let personnel = response.data;
     personnel.forEach(element => {
         valuePersonnel.innerHTML += `<option> ${element.acf.prenom} ${element.acf.nom}  </option>`;
+        // forma.innerHTML +=`
+        // <ul> 
+        //     <li>${element.acf.prenom} ${element.acf.nom}</li> 
+        //     <li>${element.acf.mail}</li>
+        //     <li>${element.acf.local}</li>
+        // </ul> `;
+        // console.log(forma);
     });
     })
     .catch(function (error) {
@@ -51,60 +66,76 @@ let estRdvOuAutre = true;
     // always executed
     }); 
 
+
+
+    console.log(formation.value);
+
+    
+
+
     let selectRdv = function(){
         valueRdv.addEventListener("change",e => {
             console.log(e.target.selectedOptions[0].dataset.id);
             if(e.target.selectedOptions[0].dataset.id === "1"){
                 formationBlock.classList.add("active");
+                console.log(e.target)
                 personnelBlock.classList.remove("active");
-                estRdvOuAutre = true;
             }else{
                 formationBlock.classList.remove("active");
                 personnelBlock.classList.add("active");
-                estRdvOuAutre = false;
             }
         })
     };
 
     selectRdv();
 
-    console.log("yes :",valuePersonnel.innerHTML);
 
     // ---------------------------------------------------------------------------------------------------------//
     // --------------------------------------------- POST ------------------------------------------------------//
-
+    
     function envoyerDonne(){
         let url = "https://exerciceintegre.firebaseio.com/Visiteur.json";
-        console.log("perso : ",perso);
-        axios.post(url, {
-        Nom : listInput[0].value ,
-        Prenom : listInput[1].value ,
-        Mail : listInput[2].value ,
-        derniereVisite :` ${dateMtn.getFullYear()} - ${dateMtn.getMonth()+1} -  ${dateMtn.getDate()}`,
-        avatar: "",
-        Visite : [
-            {
-                nomVisite :` rdv `,
-                Objet : ""
-            }
-        ]
-      })
-      .then(function (response) {
-        console.log(response);
-        let id = response.data.name;
-        console.log(generateQrcode(id));
-        if(response.status == 200){
-            container.classList.add("enabled");
-            qrcodeBlock.classList.add("active");
-            qrcodeBlock.classList.remove("qrcodeBlock");
-        }else{
-            container.classList.remove("enabled");
-            qrcodeBlock.classList.remove("active");
+        if(inputNom.value != "" && inputPrenom.value != "" && inputMail.value != ""){
+            axios.post(url, {
+            Nom : listInput[0].value ,
+            Prenom : listInput[1].value ,
+            Mail : listInput[2].value ,
+            derniereVisite :` ${dateMtn.getFullYear()} - ${dateMtn.getMonth()+1} -  ${dateMtn.getDate()}`,
+            avatar: "",
+            Visite : [
+                {
+                    nomVisite :` rdv `,
+                    Objet : ""
+                }
+            ]
+            
         }
+    )
+        .then(function (response) {
+            console.log(response);
+            let id = response.data.name;
+            console.log(generateQrcode(id));
+            
+            if(response.status == 200){
+                container.classList.add("enabled");
+                qrcodeBlock.classList.add("active");
+                qrcodeBlock.classList.remove("qrcodeBlock");
+            }else{
+                container.classList.remove("enabled");
+                qrcodeBlock.classList.remove("active");
+            }
+        
       })
+    
       .catch(function (error) {
         console.log(error);
       });
+    }
+    else{
+        messageEror.forEach(element => {
+            element.innerHTML += `${message}`;
+        });
+    }
 }
 listInput[3].addEventListener("click",envoyerDonne);
 
@@ -112,14 +143,4 @@ function generateQrcode(id){
     new QRCode(qrcode,id);
 }
 
-axios.get(`https://exerciceintegre.firebaseio.com/Visiteur/${res.data}.json`)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-});
 
